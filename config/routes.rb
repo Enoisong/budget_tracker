7351e2c053763a823df3620df710cf85e2047562
Enoisong <enobongitiaba988@gmail.com>
2023-09-26 03:36:00 -0700

    Implement style for  authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'categories#index'
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  resources :categories do
+    resources :bills, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
