1bb7485adbacd928ce6399900d1ec0da274a7bc9
Enoisong <enobongitiaba988@gmail.com>
2023-09-26 03:56:42 -0700

    Add app functionalities

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,23 @@
 class BillsController < ApplicationController
-    def index
+  def new
+    @bill = Bill.new
+    @category = Category.find(params[:category_id])
+  end
+
+  def create
+    @bill = Bill.new(bill_params)
+    @bill.author = current_user
+    @bill.categories << Category.find(params[:category_id])
+    if @bill.save
+      redirect_to category_path(params[:category_id])
+    else
+      render :new, status: :unprocessable_entity
     end
   end
-  
+
+  private
+
+  def bill_params
+    params.require(:bill).permit(:name, :amount)
+  end
+end
