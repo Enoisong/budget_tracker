3ea8fba11bf777912bee33e4c96829b676c3dc70
Enoisong <enobongitiaba988@gmail.com>
2023-09-25 20:16:26 -0700

    Add controllers

---
+++ app/controllers/bills_controller.rb
@@ -0,0 +1,5 @@
+class BillsController < ApplicationController
+    def index
+    end
+  end
+  
---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,5 @@
+class CatergoryController < ApplicationController
+    def index
+    end
+  end
+  
